5f728481309fdc079579e5abd7fba1f7e64f560e
Dinesh Pathak <dineshpthk56@gmail.com>
2020-09-20 18:54:58 +0530

    Added test cased and business logic to apply membership upgrade

---
+++ OrderManagement/OrderManagement.Tests/V1/Business/MembershipUpgradeTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using OrderManagement.Abstraction;
+using OrderManagement.Concretion;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrderManagement.Tests.V1.Business
+{
+    [TestFixture]
+    class MembershipUpgradeTests
+    {
+        private IMembershipUpgrade _iMembershipUpgrade;
+
+        [Test]
+        public void MembershipUpgrade_MembershipUpgradeSuccess_WhenUpgradeMembershipIsTrue()
+        {
+            // Arrange
+            _iMembershipUpgrade = new MembershipUpgrade();
+
+            //// Act
+            var response = _iMembershipUpgrade.UpgradeMembership(true);
+
+            //// Assert
+            response.Equals("Upgrade applied");
+        }
+
+        [Test]
+        public void MembershipUpgrade_MembershipUpgradeFails_WhenUpgradeMembershipIsFalse()
+        {
+            // Arrange
+            _iMembershipUpgrade = new MembershipUpgrade();
+
+            //// Act
+            var response = _iMembershipUpgrade.UpgradeMembership(false);
+
+            //// Assert
+            response.Equals("Upgrade failed due to an error");
+        }
+    }
+}
---
+++ OrderManagement/OrderManagement/Concretion/MembershipUpgrade.cs
@@ -0,0 +1,21 @@
+﻿using OrderManagement.Abstraction;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrderManagement.Concretion
+{
+    public class MembershipUpgrade : IMembershipUpgrade
+    {
+        public string UpgradeMembership(bool upgradeMembership)
+        {
+            if (upgradeMembership)
+            {
+                return "Upgrade applied";
+            } else
+            {
+                return "Upgrade failed due to an error";
+            }
+        }
+    }
+}
